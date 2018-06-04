073e630e28e7e319bcce2418610f5aea2fb7d095
andrewgjohnson <andrew@andrewgjohnson.com>
2018-06-04 16:59:17 -0500

    v1.0.1

--- source/linebreaks4imagettftext.php
+++ source/linebreaks4imagettftext.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Linebreaks4imagettftext v1.0.0
+ * Linebreaks4imagettftext v1.0.1
  *
  * Copyright (c) 2018 Andrew G. Johnson <andrew@andrewgjohnson.com>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
